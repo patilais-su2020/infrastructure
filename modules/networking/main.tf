360c026906ea3d60457baf3b9fda6b3f620bb6d8
Aishwarya Patil <patil.ais@northeastern.edu>
2020-06-18 20:17:32 -0400

    Changes to process.env

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -275,14 +275,14 @@ resource "aws_instance" "csye_6225_ec2" {
   iam_instance_profile    = "${aws_iam_instance_profile.ec2_profile.name}"
   user_data = <<-EOF
           #!/bin/bash
-          echo "db_hostname=${aws_db_instance.rds_instance.address}">>/home/ubuntu/.bashrc
-          echo "db_username=${var.db_master_username}">>/home/ubuntu/.bashrc
-          echo "db_password=${var.db_master_password}">>/home/ubuntu/.bashrc
-          echo "s3_bucket_name=${var.s3_bucket_name}">>/home/ubuntu/.bashrc
-          echo "prod_access_key=${var.prod_access_key}">>/home/ubuntu/.bashrc
-          echo "prod_secret_key=${var.prod_secret_key}">>/home/ubuntu/.bashrc
-          echo "prod_region=${var.region}">>/home/ubuntu/.bashrc
-          echo "db_name=${var.rds_instance_name}">>/home/ubuntu/.bashrc
+          echo "export db_hostname=${aws_db_instance.rds_instance.address}">>/home/ubuntu/.bashrc
+          echo "export db_username=${var.db_master_username}">>/home/ubuntu/.bashrc
+          echo "export db_password=${var.db_master_password}">>/home/ubuntu/.bashrc
+          echo "export s3_bucket_name=${var.s3_bucket_name}">>/home/ubuntu/.bashrc
+          echo "export prod_access_key=${var.prod_access_key}">>/home/ubuntu/.bashrc
+          echo "export prod_secret_key=${var.prod_secret_key}">>/home/ubuntu/.bashrc
+          echo "export prod_region=${var.region}">>/home/ubuntu/.bashrc
+          echo "export db_name=${var.rds_instance_name}">>/home/ubuntu/.bashrc
       EOF  
   root_block_device {
     volume_type           =  var.root_block_device_volume_type
